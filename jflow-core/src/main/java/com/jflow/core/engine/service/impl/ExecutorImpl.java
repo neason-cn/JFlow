a8ce8cc16d76fc8add6877110e2f646852c68847
neason-cn <cqwjsl123@126.com>
2022-06-16 23:45:48 +0800

    add log for Executor

--- jflow-core/src/main/java/com/jflow/core/engine/service/impl/ExecutorImpl.java
+++ jflow-core/src/main/java/com/jflow/core/engine/service/impl/ExecutorImpl.java
@@ -4,9 +4,11 @@ import com.jflow.common.exception.FlowException;
 import com.jflow.core.engine.service.Executor;
 import com.jflow.infra.spi.cache.CacheSpi;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,14 +18,25 @@ import static com.jflow.common.error.Errors.LOCK_KEY_ERROR;
  * @author neason
  * @since 0.0.1
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ExecutorImpl implements Executor {
 
-    @Value("${flow.executor.async:false}")
-    private boolean async;
+    @Value("${jflow.executor.async:false}")
+    private boolean executorMode;
+
+    @Value("${jflow.executor.pool.size:100}")
+    private int executorPoolSize;
     private final CacheSpi cacheSpi;
-    private ExecutorService executor = Executors.newFixedThreadPool(10);
+    private ExecutorService executor;
+
+    @PostConstruct
+    public void init() {
+        log.info("the mode of Executor is {} from {}", executorMode, "jflow.executor.async");
+        log.info("the pool size of Executor is {} from {}", executorPoolSize, "jflow.executor.pool.size");
+        executor = Executors.newFixedThreadPool(executorPoolSize);
+    }
 
     @Override
     public void asyncRun(String key, Runnable runnable) {
@@ -36,7 +49,7 @@ public class ExecutorImpl implements Executor {
                 throw new FlowException(LOCK_KEY_ERROR, key);
             }
 
-            if (async) {
+            if (executorMode) {
                 executor.submit(runnable);
                 return;
             }
