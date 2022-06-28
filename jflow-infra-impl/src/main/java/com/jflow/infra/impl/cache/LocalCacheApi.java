027250d12cd245b698314adb90d6dc93804aa957
neason-cn <cqwjsl123@126.com>
2022-06-28 19:30:13 +0800

    log in cache

--- jflow-infra-impl/src/main/java/com/jflow/infra/impl/cache/LocalCacheApi.java
+++ jflow-infra-impl/src/main/java/com/jflow/infra/impl/cache/LocalCacheApi.java
@@ -3,6 +3,7 @@ package com.jflow.infra.impl.cache;
 import cn.hutool.cache.Cache;
 import cn.hutool.cache.CacheUtil;
 import com.jflow.infra.spi.cache.CacheSpi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
  * @author neason
  * @since 0.0.1
  */
+@Slf4j
 @Component
 public class LocalCacheApi implements CacheSpi {
 
@@ -48,9 +50,11 @@ public class LocalCacheApi implements CacheSpi {
     @Override
     public boolean tryLock(Serializable key, long expireTime) {
         if (null != cache.get(key)) {
+            log.debug("key: {} has been locked by someone else", key);
             return false;
         }
         cache.put(key, Thread.currentThread().getName());
+        log.debug("locked key: {}", key);
         return true;
     }
 
@@ -58,14 +62,17 @@ public class LocalCacheApi implements CacheSpi {
     public boolean unLock(Serializable key) {
         Serializable value = cache.get(key);
         if (null == value) {
+            log.debug("key: {} is not locked by anyone", key);
             return true;
         }
 
         if (!Thread.currentThread().getName().equals(value.toString())) {
+            log.debug("key: {} is not locked by this thread", key);
             return false;
         }
 
         cache.remove(key);
+        log.debug("unlock key: {}", key);
         return true;
     }
 }
